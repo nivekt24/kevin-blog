8707c6c4dbd607478280780067df8c28a21758c4
nivekt24 <kvvt24@gmail.com>
2023-12-03 18:26:31 -0800

    edit profile detail/photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kevin Tran`,
+      summary: `who lives and works in Southern California building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ktrandev`,
     },
   },
   plugins: [

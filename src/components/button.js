c82a014245334d0132909cd39f003b1c4984462b
nivekt24 <kvvt24@gmail.com>
2024-01-31 19:49:52 -0800

    scale elements/add new socials

--- src/components/button.js
+++ src/components/button.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 
 const StyledLinkButton = styled.div`
   font-family: "DM Mono", monospace;
+  font-size: 0.7rem;
   font-weight: 500;
   letter-spacing: 0.03em;
   text-transform: uppercase;
@@ -11,6 +12,10 @@ const StyledLinkButton = styled.div`
 
   &:hover {
     text-decoration: underline;
+  }
+
+  @media screen and (min-width: 640px) {
+    font-size: 0.875rem;
   }
 `
 
--- src/components/title.js
+++ src/components/title.js
@@ -3,11 +3,16 @@ import styled from "styled-components"
 
 const StyledTitle = styled.div`
   font-family: "DM Mono", monospace;
+  font-size: 0.875rem;
   text-transform: uppercase;
   border-radius: 0.75rem;
   letter-spacing: 0.1em;
   font-weight: 700;
   /* padding: 4px 16px; */
+
+  @media screen and (min-width: 640px) {
+    font-size: 1rem;
+  }
 `
 
 export const Title = ({ children }) => {
